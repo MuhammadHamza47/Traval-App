4255afcd1ef72b17484e10acc31429f45ba39a00
Muhammad Hamza <hafizhamza2247@gmail.com>
2023-11-13 20:20:57 +0500

    app.tsx

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
-import React, {Component} from 'react';
-import {Text, View} from 'react-native';
 
-export default class App extends Component {
-  render() {
-    return (
-      <View>
-        <Text> textInComponent </Text>
-      </View>
-    );
-  }
-}
+import First from './src/screens/intro/first/First';
+import Second from './src/screens/intro/second/Second';
+import Third from './src/screens/intro/third/Third';
+import Splash from './src/screens/splash/Splash';
+const App = () => (
+
+  <>
+    {/* <Splash /> */}
+    {/* <First/> */}
+    <Second/>
+    {/* <Third /> */}
+  </>
+);
+
+export default App;
+
+
