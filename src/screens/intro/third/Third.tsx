512c6c552185e8707616325fc70e340eb990a41f
Muhammad Hamza <hafizhamza2247@gmail.com>
2023-11-16 21:59:20 +0500

    third.tsx

--- src/screens/intro/third/Third.tsx
+++ src/screens/intro/third/Third.tsx
@@ -1,6 +1,8 @@
 import { View, Text, Image, Button } from 'react-native'
 import React from 'react'
 import { StyleSheet } from 'react-native'
+import { Heading, Paragraph, LastText } from '../../../components/startText/Text'
+import StartBtn from '../../../components/startBtn/StartBtn'
 
 export default function Third() {
   return (
@@ -10,12 +12,12 @@ export default function Third() {
         <Image style={styles.img} source={require('../../../assets/intro3.png')} />
     </View>
     <View style={styles.container2} >
-        <Text style={styles.title}>High-end leisure projects to choose from</Text>
-        <Text style={styles.desc}>The world's first-class modern leisure and entertainment method</Text>
-        <Button  title="Next" color="#0373F3"  />
+        <Heading title="High-end leisure projects to choose from"/>
+        <Paragraph Text="The world's first-class modern leisure and entertainment method" />
+        <StartBtn />
     </View>
     <View style={styles.footer} >
-        <Text style={{marginTop: 40}}>Nordic Vacation Sponsor</Text>
+        <LastText />
     </View>
     </>
   )
@@ -48,20 +50,5 @@ const styles = StyleSheet.create({
         width: 300,
         height: 300,
         resizeMode: 'contain'
-    },
-    title:{
-        fontSize: 30,
-        textAlign: 'center',
-        fontWeight: '600',
-        color: '#000000',
-        paddingHorizontal: 20,
-        marginHorizontal: 10
-    },
-    desc:{
-        fontSize: 20,
-        textAlign: 'center',
-        padding: 15,
-        marginBottom: 20
-    },
-
+    }
 })
