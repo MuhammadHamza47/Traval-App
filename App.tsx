189dd4f87325744eead5b096f2b2c8316a1de8da
Muhammad Hamza <hafizhamza2247@gmail.com>
2023-11-22 22:48:02 +0500

    App

--- App.tsx
+++ App.tsx
@@ -3,13 +3,11 @@ import First from './src/screens/intro/first/First';
 import Second from './src/screens/intro/second/Second';
 import Third from './src/screens/intro/third/Third';
 import Splash from './src/screens/splash/Splash';
+import Navigation from './src/navigation/Navigation';
 const App = () => (
 
   <>
-    {/* <Splash /> */}
-    {/* <First/> */}
-    <Second/>
-    {/* <Third /> */}
+   <Navigation />
   </>
 );
 
